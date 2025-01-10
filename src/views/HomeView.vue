1508007cff683b265524ad07dcea336bda2e188d
Allan Paz <jallanpazg@gmail.com>
2025-01-09 17:48:28 -0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup>
-
-</script>
-
 <template>
-  <main>
-
+  <main class="container text-white">
+    <div class="pt-4 mb-8 relative">
+      <input 
+        type="text"
+        v-model="searchQuery" 
+        placeholder="Search for a city" 
+        class="w-full p-2 bg-transparent border-b focus:border-weather-secondary focus:outline-none focus:shadow-[0px_1px_0_0_#004E71] text-white" 
+      />
+    </div>
   </main>
 </template>
+
+<script setup>
+import { ref } from 'vue';
+import axios from 'axios';
+
+const mapboxAPIKey = 'pk.eyJ1Ijoiam9obmtvbWFybmlja2kiLCJhIjoiY2t5NjFz0DZvMHJkaDJ1bWx60GVieGxreSJ9.IpojdT3U3NENKnF6_WhR2Q';
+
+const searchQuery = ref('');
+const queryTimeout = ref(null);
+const mapboxSearchResults = ref(null);
+
+const getSearchResults = () => {
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== '') {
+      const result = await axios.get(`https://api.mapbox.com/geocoding/v5/mapbox.places/${searchQuery.value}.json?access_token=${mapboxAPIKey}`);
+      mapboxSearchResults.value = result.data.features;
+      return;
+    }
+    mapboxSearchResults.value = null;
+  }, 300);
+};
+</script>
